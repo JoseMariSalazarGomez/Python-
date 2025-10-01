bc3779b4046abfcc6dbe383375c96f1d16525586
José María Salazar Gómez <jsalgom1610@g.educaand.es>
2025-10-01 14:32:06 +0200

    Ejercicio 5 Hecho

---
+++ Ejercicios/Ejercicio5.py
@@ -0,0 +1,11 @@
+#EJERCICIO 4 PHYTON JOSÉ MARÍA SALAZAR GÓMEZ
+
+frase = input("Introduce una frase: ")
+letra_para_reemplazar = input("Introduce la letra que quieras: ")
+letra_reemplazo = input("Introduce la letra por la que quieres reemplazarla: ")
+
+Cantidad = frase.count(letra_para_reemplazar)
+frase_modificada = frase.replace(letra_para_reemplazar, letra_reemplazo)
+
+print(f"{Cantidad} apariciones.")
+print("Resultado:", frase_modificada)
